c79583b6d58c863e6e95c65c72c44297f115babc
Adam Schwalm <adamschwalm@gmail.com>
2013-03-19 20:31:00 -0500

    Added ConditionLists
    I have never been more proud of code.

---
+++ include/Condition/Condition.h
@@ -0,0 +1,63 @@
+#ifndef CONDITION_H_
+#define CONDITION_H_
+
+#include <memory>
+#include <utility>
+#include <functional>
+#include <vector>
+
+namespace Condition
+{
+
+	class ConditionList
+	{
+
+	public:
+
+		ConditionList(){};
+
+		template<typename T, typename U>
+		void addCondition( bool(*f)(T, U), T t, U u);
+
+
+		bool isSatisfied();
+
+	private:
+		std::vector< std::function<bool()> > conditions;
+
+	};
+
+
+	template<typename T, typename U>
+	void ConditionList::addCondition( bool (*f)(T, U), T t, U u)
+	{
+		std::function<bool()> func = [=]{ return f(t, u); };
+		conditions.push_back(func);
+	}
+
+	bool ConditionList::isSatisfied()
+	{
+		for (auto c : conditions)
+		{
+			if (!c())
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	template<typename T, typename U>
+	bool HAS_ATTRIBUTE(T t, U u)
+	{
+		if (t->hasAttribute(u))
+			return true;
+		return false;
+	}
+
+
+
+
+}
+#endif
--- scr/main.cpp
+++ scr/main.cpp
@@ -14,38 +14,51 @@
 #include "Time/DateManager.h"
 #include "Time/TimeManager.h"
 #include "Relationship/Relationship.h"
+#include "Condition/Condition.h"
 
 #include <cassert>
 #include <iostream>
+#include <utility>
 
 int main()
 {
-
-
-	auto l = std::make_shared<Location::Village>(Location::World::getInstance(), "test Location");
-	auto l2 = std::make_shared<Location::Village>(l, "test Location2");
-
-	std::cout << "l at "  << l.get() << std::endl;
-	for (auto location : l->getLocations())
-		std::cout << location->getName() << std::endl;
-	std::cout << std::endl;
-	std::cout << "l2 at " << l2.get() << std::endl;
-	for (auto location : l2->getLocations())
-		std::cout << location->getName() << std::endl;
-	std::cout << std::endl;
-
-	auto item = std::make_shared<Item::BaseItem>();
+	Item::Item_ptr item = std::make_shared<Item::BaseItem>();
 	item->setAttribute("edible");
 
+	Condition::ConditionList c;
 
-	for (auto item : l2->getItems())
+	c.addCondition(Condition::HAS_ATTRIBUTE, item, "edible");
+
+	if (c.isSatisfied())
 	{
-		std::cout << item->getName() << std::endl;
+		std::cout << "success" << std::endl;
 	}
 
 
-	auto p = std::make_shared<Individual::Person>("test Person", l);
-	p->addGoal(Action::GoalCreator::getInstance().createGoal(Action::GoalType::GET_FOOD, p, 0));
+//	auto l = std::make_shared<Location::Village>(Location::World::getInstance(), "test Location");
+//	auto l2 = std::make_shared<Location::Village>(l, "test Location2");
+//
+//	std::cout << "l at "  << l.get() << std::endl;
+//	for (auto location : l->getLocations())
+//		std::cout << location->getName() << std::endl;
+//	std::cout << std::endl;
+//	std::cout << "l2 at " << l2.get() << std::endl;
+//	for (auto location : l2->getLocations())
+//		std::cout << location->getName() << std::endl;
+//	std::cout << std::endl;
+//
+//	auto item = std::make_shared<Item::BaseItem>();
+//	item->setAttribute("edible");
+//
+//
+//	for (auto item : l2->getItems())
+//	{
+//		std::cout << item->getName() << std::endl;
+//	}
+//
+//
+//	auto p = std::make_shared<Individual::Person>("test Person", l);
+//	p->addGoal(Action::GoalCreator::getInstance().createGoal(Action::GoalType::GET_FOOD, p, 0));
 
 	return 0;
 }
