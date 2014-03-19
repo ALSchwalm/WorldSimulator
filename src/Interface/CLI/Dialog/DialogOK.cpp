48c12b91b13091ead046ba0055a108a3da0bacee
Adam Schwalm <adamschwalm@gmail.com>
2014-03-19 16:17:27 -0500

    Fixed bug where contents of dialogOK would not render sometimes

--- src/Interface/CLI/Dialog/DialogOK.cpp
+++ src/Interface/CLI/Dialog/DialogOK.cpp
@@ -6,21 +6,25 @@
 using namespace Interface::CLI;
 
 DialogOK::DialogOK(std::string displayText) :
-        Dialog(subwin(mainwin, 3, displayText.size()+4, LINES/2, COLS/2-(displayText.size()/2)))
+    Dialog(subwin(mainwin, 3, displayText.size()+4, LINES/2, COLS/2-(displayText.size()/2))),
+    text(displayText)
 {
     mvwprintw(dialogWin, 0, 2, "Error");
     mvwprintw(dialogWin, 1, 1, displayText.c_str());
-    wrefresh(dialogWin);
+    wrefresh(mainwin);
 }
 
 void DialogOK::handleInput()
 {
+    box(dialogWin, 0, 0);
+    mvwprintw(dialogWin, 0, 2, "Error");
+    mvwprintw(dialogWin, 1, 1, text.c_str());
+    wrefresh(mainwin);
     char c = wgetch(dialogWin);
     if (c == 10)
     {
         wclear(dialogWin);
         wrefresh(dialogWin);
         dialogs.pop_back();
-
     }
 }
