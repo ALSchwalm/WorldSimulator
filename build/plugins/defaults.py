609b1d5823a957bff30a2553a6f6a301044e95bb
Adam Schwalm <adamschwalm@gmail.com>
2014-03-19 21:52:11 -0500

    Added build folder with plugins

---
+++ build/plugins/defaults.py
@@ -0,0 +1,43 @@
+from simulator import *
+from abc import abstractmethod
+
+class Item(BaseItem):
+    related_items = []
+    default_attributes = {"a" : False}
+    def __init__(self):
+        BaseItem.__init__(self)
+
+
+class Knife(Item):
+    def __init__(self):
+        Item.__init__(self, Knife.__name__)
+
+    def make():
+        self.attributes = {"a" : True}
+
+    default_attributes = {"a" : False}
+
+    def getRequiredItems(self):
+        return [
+            (Dagger, lambda item: True)
+        ]
+
+
+class Dagger(Item):
+    related_items = [Knife]
+    def __init__(self):
+        Item.__init__(self, Dagger.__name__)
+        self.attributes = {"a" : False}
+
+    default_attributes = {"a" : False}
+
+    def getRequiredItems(self):
+        return [
+            (Dagger, lambda item: item.attributes["a"])
+        ]
+
+class House(BaseContainer):
+    def __init__(self):
+        BaseContainer.__init__(self, House.__name__)
+
+    default_attributes = {"habitable" : False}
